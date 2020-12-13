9e6fe4831d6a5213c140a733212c70063f8db3db
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2020-12-13 20:55:15 +0000

    font change

--- data/stocks.js
+++ data/stocks.js
@@ -2,4 +2,5 @@ export default [
     {id: 1, name:'MeroBhakaari', price: 110},
     {id: 2, name:'Spinel Tech', price: 100},
     {id: 3, name:'Isop Nepal', price: 120},
-    {id: 4, name:'Neptrox', price: 1
+    {id: 4, name:'Neptrox', price: 150},
+]
