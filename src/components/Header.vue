cf86c8e6cb690320a9ba83777205581b7c1e0d6a
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-01-12 09:05:11 +0000

    Update pods.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/" class="navbar-brand">Stock Trader</rou
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <button
+      class="navbar-toggler"
+ 
