cffae47561f7ec47283f7952bda9a8f389071627
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-03-12 06:55:41 +0000

    Set theme jekyll-theme-leap-day

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,4 +2,5 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to confi
+      For a guide and recipes on how to configure / customize this project,<br />
+      check ou
