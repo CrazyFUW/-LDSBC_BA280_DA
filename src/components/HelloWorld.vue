327b6bcfac2a5acb94464a8a43eb8c0807e0607f
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-03-12 10:04:07 +0000

    Update match_results.html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,4 +3,5 @@
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
-      check ou
+      check out the
+      <a href="https://cli.vuejs.org"
