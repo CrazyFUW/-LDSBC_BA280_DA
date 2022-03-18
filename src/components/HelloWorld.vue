32c853fccfff7e24e57391bd9f01aa8b4ee27b7f
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-03-18 14:16:33 +0000

    Update dependencies:zap::zap:

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,4 +13,6 @@
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-  
+          target="_blank"
+          rel="noopener"
+          >b
