b2303e013ce3de1a2327520090503e74713bf240
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-05-05 21:23:50 +0000

    remove bug

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,6 @@
 <template>
   <div class="row">
-  <app-stock v-for="stock in stocks" :key="stock.id" :stock="sto
+  <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
+</template>
+
