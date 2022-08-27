88ac04da1e945cf72ab1a1c45a64345f4076eaeb
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-08-27 21:19:58 +0000

    check error

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../components/Home";
-import Portfolio from "../compo
+import Portfolio from "../components/portfolio/Portfolio";
+import Stocks
