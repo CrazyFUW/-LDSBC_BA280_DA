844eebc98392c4c71d2aea47a5125a40a9c98528
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-08-29 13:19:42 +0000

    added new files

--- src/router/index.js
+++ src/router/index.js
@@ -2,4 +2,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../components/Home";
 import Portfolio from "../components/portfolio/Portfolio";
-import Stocks
+import Stocks from "../components/stocks/Stocks";
+
+Vue.use(VueRouter
