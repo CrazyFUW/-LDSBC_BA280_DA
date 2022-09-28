712d3ebf8c92b7ad911f9091960ea301b20f74f0
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-09-28 06:09:31 +0000

    Save work.

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -5,4 +5,5 @@ const state = {
 
 const mutations = {
     'BUY_STOCK' (state, {stockId, quantity, stockPrice}) {
-        const record = sta
+        const record = state.stocks.find(element => element.id == stockId);
+      
