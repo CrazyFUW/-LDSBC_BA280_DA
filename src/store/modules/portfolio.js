7b7bbfe46e4ea0b061575a826afff926cc9c2f0b
CrazyFUW <95767371+CrazyFUW@users.noreply.github.com>
2022-11-12 07:34:26 +0000

    and one more

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -40,4 +40,4 @@ const actions = {
 const getters = {
     stockPortfolio (state, getters) {
         return state.stocks.map(stock => {
-            const
+            const record = getters.stocks.find(element => ele
